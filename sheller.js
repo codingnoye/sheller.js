7052a5526fc08110aeda32542227a2bc11751f6b
codingnoye <codingnoye@kookmin.ac.kr>
2019-12-28 04:26:52 +0900

    0.1.0

---
+++ sheller.js
@@ -0,0 +1,12 @@
+const exec = require('child_process').exec
+
+const sheller = (cmd) => {
+    return new Promise((res, rej) => {
+	exec(cmd, (err, stdout, stderr) => {
+	    if (err | stderr) rej(err | stderr)
+	    else res(stdout)
+	})
+    })
+}
+
+module.exports = sheller
